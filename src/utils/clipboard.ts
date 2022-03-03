4f6ddc6018d6d633b766c5f8ab3fd65c677c1fc0
Daniel Qu <danielq987@gmail.com>
2022-03-03 02:50:43 -0500

    Convert indentation to spaceS

--- src/utils/clipboard.ts
+++ src/utils/clipboard.ts
@@ -1,8 +1,8 @@
 // Holy this is yucky
 const colorMapping: any = {
-	"rgb(144, 238, 144)": "🟩",
-	"rgb(255, 252, 187)": "🟨",
-	"rgb(225, 225, 225)": "⬜",
+  "rgb(144, 238, 144)": "🟩",
+  "rgb(255, 252, 187)": "🟨",
+  "rgb(225, 225, 225)": "⬜",
 };
 
 /**
@@ -13,27 +13,27 @@ const colorMapping: any = {
  * @param hexleNumber Number associated with the daily hexle.
  */
 const getClipboardText = (letters: string[][], isWin: boolean, hexleNumber: number): string => {
-	let tries: string | number;
-	if (!isWin) tries = "X";
-	else {
-		tries = letters.findIndex((row) => row[0] === "");
-		// If empty row not found, this is a win in 6 guesses.
-		tries = tries === -1 ? 6 : tries;
-	}
-	let lettersText = `Hexle${hexleNumber} ${tries}/6\n`;
+  let tries: string | number;
+  if (!isWin) tries = "X";
+  else {
+    tries = letters.findIndex((row) => row[0] === "");
+    // If empty row not found, this is a win in 6 guesses.
+    tries = tries === -1 ? 6 : tries;
+  }
+  let lettersText = `Hexle${hexleNumber} ${tries}/6\n`;
 
-	lettersText += letters.reduce((prevValue, row, i) => {
-		const squares = row
-			.map((letter, j) => {
-				const color = document.getElementById(i.toString() + j.toString())!
-					.style.backgroundColor;
-				return colorMapping[color] || "⬜";
-			})
-			.join("");
-		return `${prevValue}${squares}\n`;
-	}, "");
+  lettersText += letters.reduce((prevValue, row, i) => {
+    const squares = row
+      .map((letter, j) => {
+        const color = document.getElementById(i.toString() + j.toString())!
+          .style.backgroundColor;
+        return colorMapping[color] || "⬜";
+      })
+      .join("");
+    return `${prevValue}${squares}\n`;
+  }, "");
 
-	return lettersText;
+  return lettersText;
 };
 
 export { getClipboardText };
