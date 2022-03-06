57abd390697573cb1a47c026545b8d503f6a467a
Daniel Qu <danielq987@gmail.com>
2022-03-06 03:25:33 -0500

    Remove extra whitespace in clipboard output

--- src/utils/clipboard.ts
+++ src/utils/clipboard.ts
@@ -22,7 +22,7 @@ const getClipboardText = (letters: string[][], isWin: boolean, hexleNumber: numb
     // If empty row not found, this is a win in 6 guesses.
     tries = tries === -1 ? 6 : tries;
   }
-  let lettersText = `Hexle ${hexleNumber} ${tries}/6\n https://frankljin.github.io/hexle/\n`;
+  let lettersText = `Hexle ${hexleNumber} ${tries}/6\nhttps://frankljin.github.io/hexle/\n`;
 
   console.log(letters)
 
